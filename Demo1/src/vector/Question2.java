8b695dbe2c73e861c2a6bb8387879260ef7ff090
HP <HP@DESKTOP-GR18M7T>
2021-06-14 11:33:37 +0530

    updated changes

--- Demo1/src/vector/Question2.java
+++ Demo1/src/vector/Question2.java
@@ -7,8 +7,8 @@ public class Question2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Vector v= new Vector();
-		Student s= new Student("vikas","ECE","1234ABCD","Global");
-		Hod d= new Hod("vikky","004","Ece","Global");
+		Student s= new Student("vikas","ECE","1234ABCD","ABC");
+		Hod d= new Hod("vikas","123","EEE","ABC");
 		Teacher t= new Teacher("raju","ECE","Java","Global");
 		v.add(s);
 		v.add(d);
